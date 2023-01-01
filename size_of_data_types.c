fb9fdaf4fd091b87f60c5e5f4854e84e1f3c8504
N.R.M Raiyan <114010735+NRMRaiyan@users.noreply.github.com>
2023-01-01 22:42:33 +0600

    Create size_of_data_types.c

---
+++ size_of_data_types.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  
+  printf("Size of char: %ld\n", sizeof(char));
+  printf("Size of short: %ld\n", sizeof(short));
+  printf("Size of int: %ld\n", sizeof(int));
+  printf("Size of long: %ld\n", sizeof(long));
+  printf("Size of long long: %ld\n", sizeof(long long));
+  printf("Size of float: %ld\n", sizeof(float));
+  printf("Size of double: %ld\n", sizeof(double));
+  printf("Size of long double: %ld\n", sizeof(long double));
+
+  return 0;
+}
