22f0cfbe35e40f1845ddf27d88bcc592f1c7ee58
N.R.M Raiyan <114010735+NRMRaiyan@users.noreply.github.com>
2023-01-01 22:30:27 +0600

    Create ascii_value.c

---
+++ ascii_value.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+  char alphabet;
+  printf("Enter your input: ");
+  scanf("%c", &alphabet);
+  printf("ASCII value of your input is: %d", alphabet);
+  return 0;
+}
