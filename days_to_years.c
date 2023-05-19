ecba092ffcdf1ca00974dda911812b4aa87501b5
Nafizur Rahman Mallick <114010735+NRMRaiyan@users.noreply.github.com>
2023-05-20 00:54:08 +0600

    Update days_to_years.c

--- days_to_years.c
+++ days_to_years.c
@@ -15,8 +15,6 @@ int main() {
   days_in_months = (months * 30);
   remaining_days_months = remaining_days - days_in_months;
 
-  
-
   printf("Total days is %d.\n years %d.\n days in years %d.\n remaining days %d.\n months %d.\n days in months %d.\n remaining days in months %d.", days, years, days_in_years, remaining_days, months, days_in_months, remaining_days_months);
   
   return 0;
